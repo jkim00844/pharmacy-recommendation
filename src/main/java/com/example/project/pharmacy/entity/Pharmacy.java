e4e2dd7b17410b6346b6d13f05a0b603bfc1e3ba
jkim00844 <jkim00844@gmail.com>
2023-02-06 21:03:57 +0900

    JPA Auditing > BaseTimeEntity

--- src/main/java/com/example/project/pharmacy/entity/Pharmacy.java
+++ src/main/java/com/example/project/pharmacy/entity/Pharmacy.java
@@ -1,5 +1,6 @@
 package com.example.project.pharmacy.entity;
 
+import com.example.project.BaseTimeEntity;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,7 +15,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Pharmacy {
+public class Pharmacy extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
