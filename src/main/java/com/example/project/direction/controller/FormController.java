d7e2b8a663eb08ba91648cd376cecea07a6226f7
jkim00844 <jkim00844@gmail.com>
2023-02-08 21:58:33 +0900

    4. MockMvc를 이용한 테스트 코드 - FormControllerTest

--- src/main/java/com/example/project/direction/controller/FormController.java
+++ src/main/java/com/example/project/direction/controller/FormController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -18,12 +19,12 @@ public class FormController {
         return "main";
     }
 
-    @GetMapping("/search")
+    @PostMapping("/search")
     public ModelAndView postDirection(@ModelAttribute InputDto inputDto){
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("output");
-        modelAndView.addObject("outputList",
+        modelAndView.addObject("outputFormList",
             pharmacyRecommendationService.recommendPharmacyList(inputDto.getAddress()));
         return modelAndView;
     }
