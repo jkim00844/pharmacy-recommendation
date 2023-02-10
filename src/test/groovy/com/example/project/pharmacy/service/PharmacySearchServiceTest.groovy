a4d0f4a0d9b789d297bc9e7ffb069460b6d19ccc
jkim00844 <jkim00844@gmail.com>
2023-02-11 00:16:08 +0900

    4. redis 테스트 코드 작성 - redis save and find - PharmacyRedisTemplateServiceTest

---
+++ src/test/groovy/com/example/project/pharmacy/service/PharmacySearchServiceTest.groovy
@@ -0,0 +1,46 @@
+package com.example.project.pharmacy.service
+
+import com.example.project.pharmacy.cache.PharmacyRedisTemplateService
+import com.example.project.pharmacy.entity.Pharmacy
+import org.testcontainers.shaded.com.google.common.collect.Lists
+import spock.lang.Specification
+
+class PharmacySearchServiceTest extends Specification {
+
+    private PharmacySearchService pharmacySearchService
+
+    private PharmacyRepositoryService pharmacyRepositoryService = Mock()
+    private PharmacyRedisTemplateService pharmacyRedisTemplateService = Mock()
+
+    private List<Pharmacy> pharmacyList
+
+    def setup() {
+        pharmacySearchService = new PharmacySearchService(pharmacyRepositoryService, pharmacyRedisTemplateService)
+
+        pharmacyList = Lists.newArrayList(
+                Pharmacy.builder()
+                        .id(1L)
+                        .pharmacyName("호수온누리약국")
+                        .latitude(37.60894036)
+                        .longitude(127.029052)
+                        .build(),
+                Pharmacy.builder()
+                        .id(2L)
+                        .pharmacyName("돌곶이온누리약국")
+                        .latitude(37.61040424)
+                        .longitude(127.0569046)
+                        .build())
+    }
+
+    def "레디스 장애시 DB를 이용하여 약국 데이터 조회"() {
+        when:
+        pharmacyRedisTemplateService.findAll() >> []
+        pharmacyRepositoryService.findAll() >> pharmacyList
+
+        def result = pharmacySearchService.searchPharmacyDtoList()
+
+        then:
+        // 레디스는 null 리턴, 위에서 추가한 2개 약국만 조회
+        result.size() == 2
+    }
+}
